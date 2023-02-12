32a1ea2a651a6bd0fd603172410c86f6dcb2a86a
manikantaaratikatla <manikanta.aratikatla@gmail.com>
2023-02-12 22:19:08 +0530

    first

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,34 @@
+<section>
+	<div class="header flex py-2 border-b-2 items-center justify-center bg-gray-900">
+		<img src="/logo.jpeg" alt="logo" class="w-[100px] mx-6 rounded-full" />
+		<div>
+			<h1 class="text-center text-white">BABA INSTITUTE OF TECHONOLOY AND SCIENCES</h1>
+			<h6
+				class="text-center text-xl 3xs:text-[6.5px] 2xs:text-[7.5px] xs:text-[9.5px] leading-3 xl:text-lg lg:text-sm md:text-[12px] text-white"
+			>
+				( Accredited by NAAC | Approved by AICTE,New Delhi | Affiliated to JNTU-Kakinada,GV )<br
+				/>(An ISO 9001:2015 Certified Educational Institution)
+			</h6>
+		</div>
+	</div>
+	<ul class="navitems flex justify-evenly items-center  border-b-4">
+		<li class="hover:bg-red-200  py-2 rounded-3xl cursor-pointer">
+			<a class="p-2 px-6" href="/#">Home</a>
+		</li>
+		<li class="hover:bg-red-200 py-2 rounded-3xl cursor-pointer">
+			<a class="p-2 px-6" href="/#">Tournments</a>
+		</li>
+		<li class="hover:bg-red-200  py-2 rounded-3xl cursor-pointer">
+			<a class="p-2 px-6" href="/#">Events</a>
+		</li>
+		<li class="hover:bg-red-200  py-2 rounded-3xl cursor-pointer">
+			<a class="p-2 px-6" href="/#">Gallery</a>
+		</li>
+		<li class="hover:bg-red-200 py-2  rounded-3xl cursor-pointer">
+			<a class="p-2 px-6" href="/#">About</a>
+		</li>
+		<li class="hover:bg-red-200  py-2 rounded-3xl cursor-pointer">
+			<a class="p-2 px-6" href="/login">Login</a>
+		</li>
+	</ul>
+</section>
---
+++ src/routes/login/components/login.svelte
@@ -0,0 +1,50 @@
+<section class="">
+	<div class="container px-6 py-12 h-full">
+		<div class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+			<div class="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
+				<img src="/login.png" class="w-full" alt="Phone image" />
+			</div>
+			<div class="md:w-8/12 lg:w-5/12 lg:ml-20">
+				<h1 class="text-center mb-10 font-bold text-4xl">LOGIN</h1>
+				<form>
+					<!-- Email input -->
+					<div class="mb-6">
+						<input
+							type="text"
+							class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+							placeholder="Login Id"
+						/>
+					</div>
+
+					<!-- Password input -->
+					<div class="mb-6">
+						<input
+							type="password"
+							class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+							placeholder="Password"
+						/>
+					</div>
+
+					<div class="flex justify-between items-center mb-6">
+						<div class="form-group form-check" />
+						<a
+							href="#!"
+							class="text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out"
+							>Forgot password?</a
+						>
+					</div>
+
+					<!-- Submit button -->
+					<button
+						type="submit"
+						class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+						data-mdb-ripple="true"
+						data-mdb-ripple-color="light"
+					>
+						Sign in
+					</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</section>
